e002c89c99577bde9917116ab1b901401d4dd8da
eduardo vinicius <dudustyle2@hotmail.com>
2019-09-13 21:26:46 -0300

    Primeiro Commit

---
+++ com/eduardo/operacoes.py
@@ -0,0 +1,15 @@
+class Operacoes():
+
+    def soma(self, valores):
+        val = 0
+        for v in valores:
+            val = val + v
+            
+        return val
+        
+        
+    def multi(self, valores):
+        val = 2
+        for v in valores:
+            val = val * v
+        return val
---
+++ com/eduardo/test/test_operacoes.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from com.eduardo.operacoes import Operacoes
+
+class TestOperacoes(TestCase):
+
+    def setUp(self):
+        self.operacoes = Operacoes()
+		
+    def test_soma(self):
+        self.assertEqual(self.operacoes.soma([1,5]), 6, "Should be 6")
+    
+    def test_multi(self):
+        self.assertEqual(self.operacoes.multi([1,4]), 8, "Should be 8")
